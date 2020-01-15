c5cbf8b11cf14ba04c6ab9c50b048238d1cb8434
Sebastiaan Jahnke <91jase1bif@hft-stuttgart.de>
2020-01-15 23:40:51 +0100

    add signup & login

---
+++ includes/signup.inc.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['signup-submit'])) {
+
+require 'dbh.inc.php';
+
+$username = $_POST['uid'];
+$email = $_POST['pwd'];
+$password = $_POST['uid'];
+$passwordrepeat = $_POST['pwd-repeat'];
+
+
+
+
+}
